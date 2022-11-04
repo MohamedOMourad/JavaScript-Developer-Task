21d9824a0a75b00f0e7d40f4cb6e4816e38734c3
MohamedOMourad <mohamed.o.murad@gmail.com>
2022-11-04 13:51:33 +0200

    complete setup

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,21 @@
-import express from "express";
+import express, { json, urlencoded } from "express";
 import cors from "cors";
+import morgan from "morgan";
+import helmet from "helmet";
 import { readFileSync } from 'fs'
 import { getWords } from "../model/test_model";
+
 const app = express();
 
 app.use(cors({
     origin: 'http//localhost:3000'
 }))
+app.use(morgan("dev"));
+app.use(helmet());
+app.use(json());
+app.use(urlencoded({ extended: false }));
+
+
 
 // app.use('/',dataPage)
 
@@ -15,7 +24,6 @@ app.get('/', (req, res) => {
 })
 
 
-app.use(express.json());
 app.listen(8000, () => {
     console.log("server is running on port: 8000")
 })
